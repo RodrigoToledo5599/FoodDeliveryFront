36b9c543a115955392cb421e800e7b48da70e54c
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2024-05-13 22:34:24 -0300

    finalmente fiz essa bagaça de formulario funcionar

--- src/app/components/login-card/login-card.component.ts
+++ src/app/components/login-card/login-card.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { BASE_URL } from '../../../api/api';
 import { HttpClient } from '@angular/common/http';
-import { LoginForm } from './interfaces';
-
+import { LoginForm,Token } from './interfaces';
 
 @Component({
   selector: 'app-login-card',
@@ -12,25 +11,26 @@ import { LoginForm } from './interfaces';
 
 
 export class LoginCardComponent implements OnInit{
+  constructor(public http: HttpClient){}
 
 
   url = BASE_URL + 'api/login'
-  http = Inject(HttpClient)
-
-
-  loginData :LoginForm = {
-    email:'',
-    password:''
+  loginform: LoginForm ={
+    email: 'john@example.com',
+    password:'password'
   }
 
-  loginIn(data: LoginForm){
-    // console.log('chegou aq')
-    console.log(data.email)
-    this.http.post(this.url,data)
-      // .subscribe(response => {
-      //   console.log("data posted",response)
-      // })
-  }
+
+  loginIn (data:LoginForm){
+
+    console.log(data)
+    console.log(this.url)
+
+    this.http.post<Token>(this.url,data)
+      .subscribe((response:Token ) => {
+        console.log('Data posted:', response);
+      });
+    }
 
   ngOnInit(): void {
     console.log(this.url)
