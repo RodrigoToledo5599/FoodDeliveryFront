f0f2e2e4c4cb84fa30ed9f249224a825dd79bba3
RodrigoToledo5599 <rodrigotmt89@gmail.com>
2024-09-19 18:11:47 -0300

    progresso com o service de localstorage

--- src/app/local-storage.service.ts
+++ src/app/local-storage.service.ts
@@ -11,16 +11,35 @@ export class LocalStorageService {
 
   AdicionarASacola(item: Prato): void {
     let pratos: Prato[] = [];
-    pratos = JSON.parse(localStorage.getItem(this.pedidokey)!);
+    pratos = JSON.parse(localStorage.getItem(this.pedidokey)!) == null ? [] : JSON.parse(localStorage.getItem(this.pedidokey)!);
+    console.log(pratos);
     pratos.push(item);
     localStorage.setItem(this.pedidokey, JSON.stringify(pratos));
+    console.log("chegou aq");
+  }
+
+  // testando
+  // Testing(): Prato[]{
+  Testing(){
+    let lista: Prato[] = [
+      {id: "1", name: "Pizza", price: "6666", description: "jiaowdjioawjiojawd"},
+      {id: "2", name: "x burguer", price: "55", description: "aiojawd"},
+      {id: "3", name: "wjiaiwjiwij", price: "66", description: "23423ki32r2i2"},
+      {id: "4", name: "oakwkod", price: "6", description: "44444444444wdjioawjiojawd"},
+    ];
+    this.AdicionarASacola(lista[1]);
+    // this.AdicionarASacola(lista[2]);
+
+    var result = this.getPedido();
+    // var result = lista;
+    return result;
   }
 
   getPedido(): Prato[] {
     const storedItems = localStorage.getItem(this.pedidokey);
-    return storedItems ? JSON.parse(storedItems) : [];
+    return JSON.parse(storedItems!) ;
   }
 
-  
+
 
 }
--- src/app/pages/confirmar-pedido/confirmar-pedido.component.ts
+++ src/app/pages/confirmar-pedido/confirmar-pedido.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../../local-storage.service';
+import { Prato } from './../../interfaces/interfaces';
 
 @Component({
   selector: 'app-confirmar-pedido',
   templateUrl: './confirmar-pedido.component.html',
   styleUrl: './confirmar-pedido.component.css'
 })
-export class ConfirmarPedidoComponent {
 
-  constructor(
-    LSS : LocalStorageService,
-  )
-  {}
-
+export class ConfirmarPedidoComponent implements OnInit {
   
+  teste : Prato[] = [];
+  constructor(
+    private LSS : LocalStorageService,
+  )
+  {
+    
+  }
+  ngOnInit(){
+    this.teste = this.LSS.Testing();
+    
+  }
+
 }
